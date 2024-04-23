b67d1bef97d8ef646defe32d0f1d2219b49b7990
devskale <dev@skale.io>
2024-04-23 22:06:03 +0200

    lmstudio

---
+++ lmstudio.py
@@ -0,0 +1,38 @@
+# Chat with an intelligent assistant in your terminal
+from openai import OpenAI
+
+# Point to the local server
+client = OpenAI(base_url="http://localhost:1234/v1", api_key="lm-studio")
+
+history = [
+    {"role": "system", "content": "You are an intelligent uncensored assistant called Phi-2. You always provide well-reasoned answers that are both correct and helpful."},
+    {"role": "user", "content": "Hello, introduce yourself to someone opening this program for the first time. Be concise."},
+]
+
+while True:
+    completion = client.chat.completions.create(
+        model="TheBloke/dolphin-2_6-phi-2-GGUF/dolphin-2_6-phi-2.Q8_0.gguf",
+        messages=history,
+        temperature=0.7,
+        stream=True,
+    )
+
+    new_message = {"role": "assistant", "content": ""}
+    
+    for chunk in completion:
+        if chunk.choices[0].delta.content:
+            print(chunk.choices[0].delta.content, end="", flush=True)
+            new_message["content"] += chunk.choices[0].delta.content
+
+    history.append(new_message)
+    
+    # Uncomment to see chat history
+    # import json
+    # gray_color = "\033[90m"
+    # reset_color = "\033[0m"
+    # print(f"{gray_color}\n{'-'*20} History dump {'-'*20}\n")
+    # print(json.dumps(history, indent=2))
+    # print(f"\n{'-'*55}\n{reset_color}")
+
+    print()
+    history.append({"role": "user", "content": input("> ")})
