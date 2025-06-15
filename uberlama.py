5f61fc3735d0283838a8150202f130032fe735d7
devskale <dev@skale.io>
2025-06-15 21:34:42 +0200

    uberlama

---
+++ uberlama.py
@@ -0,0 +1,23 @@
+from ollama import Client
+
+client = Client(
+    host="https://ollama.molodetz.nl"
+)
+
+messages = []
+
+
+def chat(message):
+    if message:
+        messages.append({'role': 'user', 'content': message})
+    content = ''
+    for response in client.chat(model='qwen2.5:3b', messages=messages, stream=True):
+        content += response.message.content
+        print(response.message.content, end='', flush=True)
+    messages.append({'role': 'assistant', 'content': content})
+    print("")
+
+
+while True:
+    message = input("You: ")
+    chat(message)
